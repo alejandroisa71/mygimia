b150b7ed20cbb143e9027af2702cb2108e084770
alejandro <alejandroisa71@gmail.com>
2023-10-18 19:35:30 -0300

    first commit

---
+++ src/app/result/Button.jsx
@@ -0,0 +1,10 @@
+'use client'
+const Button = () => {
+  return (
+    <div>
+      <button onClick={() => console.log("hola")}>Back</button>
+    </div>
+  );
+};
+
+export default Button;
---
+++ src/app/result/page.jsx
@@ -0,0 +1,17 @@
+import Button from "./Button"
+
+const ResultPage = () => {
+  return (
+    <div className="bg-zinc-950 p-4 text-slate-400">
+      <h1>Hi Marco</h1>
+      <h2>Check your history</h2>
+      <li>1. 01/10/2023</li>
+      <li>2. 01/09/2023</li>
+      <li>3. 01/08/2023</li>
+      <li>4. 01/17/2023</li>
+      <Button/>
+    </div>
+  )
+}
+
+export default ResultPage
