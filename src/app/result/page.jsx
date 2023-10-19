c336798caa317fbd016b4f8dd23f54305bf4e62a
alejandro <alejandroisa71@gmail.com>
2023-10-19 19:43:35 -0300

    odify component result

--- src/app/result/page.jsx
+++ src/app/result/page.jsx
@@ -1,17 +1,26 @@
-import Button from "./Button"
+import Button from "./Button";
+import Link from 'next/link'
 
 const ResultPage = () => {
+  const results =[
+    {id:1, date:'01/07/2023'},
+    {id:2, date:'01/08/2023'},
+    {id:3, date:'01/09/2023'},
+    {id:4, date:'01/10/2023'},
+  ]
   return (
     <div className="bg-zinc-950 p-4 text-slate-400">
       <h1>Hi Marco</h1>
       <h2>Check your history</h2>
-      <li>1. 01/10/2023</li>
-      <li>2. 01/09/2023</li>
-      <li>3. 01/08/2023</li>
-      <li>4. 01/17/2023</li>
-      <Button/>
+      <ul className="bg-red-500 text-slate-800"> 
+        {results.map( result =>{
+          <li>{result.date}</li>
+        })}
+        <li><Link href='/'>Home</Link></li>
+      </ul>
+      <Button />
     </div>
-  )
-}
+  );
+};
 
-export default ResultPage
+export default ResultPage;
