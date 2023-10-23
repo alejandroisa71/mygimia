c23b904f60cb8ae751cd3d12de4413ab4aad874b
alejandro <alejandroisa71@gmail.com>
2023-10-23 20:30:00 -0300

    Add file for download

--- src/app/components/ResultCard.jsx
+++ src/app/components/ResultCard.jsx
@@ -2,12 +2,14 @@ import Link from "next/link";
 
 const ResultCard = ({ result }) => {
   console.log(result);
+  const {date}= result
   return (
     <div className=" m-2 flex hover:bg-orange-400">
       <p>
         {result.id}. {result.date}
       </p>
-      <Link href={`/results/${result.id}`}>Download</Link>
+      <Link href="/01-09-2023.pdf" target='_blank' download='01-09-2023.pdf'>Download</Link>
+      {/* href={`/results/${result.id}`} */}
     </div>
   );
 };
--- src/app/results/[resultId]/page.jsx
+++ src/app/results/[resultId]/page.jsx
@@ -2,7 +2,9 @@
 const Page = ({params}) => {
     
   return (
-    <div>{params.resultId}</div>
+    <div>{params.resultId}
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium laboriosam blanditiis eveniet ipsam iusto autem libero, accusamus aspernatur cupiditate sequi!</p>
+    </div>
   )
 }
 
