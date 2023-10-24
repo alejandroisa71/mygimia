f9e2964221b5077d3facdbe5e0e5496dc059927f
Alejandro <alejandroisa71@gmail.com>
2023-10-24 02:09:02 -0300

    passin our projetc

--- src/app/components/ResultCard.jsx
+++ src/app/components/ResultCard.jsx
@@ -2,14 +2,15 @@ import Link from "next/link";
 
 const ResultCard = ({ result }) => {
   console.log(result);
-  const {date}= result
+  const { date } = result;
   return (
-    <div className=" m-2 flex hover:bg-orange-400">
-      <p>
-        {result.id}. {result.date}
-      </p>
-      <Link href="/01-09-2023.pdf" target='_blank' download='01-09-2023.pdf'>Download</Link>
-      {/* href={`/results/${result.id}`} */}
+    <div className="container flex justify-around  hover:bg-orange-400 md:text-2xl">
+        <p>
+          {result.id}. {result.date}
+        </p>
+        <Link href="/01-09-2023.pdf" target="_blank" download="01-09-2023.pdf">
+          Download
+        </Link>
     </div>
   );
 };
--- src/app/results/page.jsx
+++ src/app/results/page.jsx
@@ -1,5 +1,4 @@
 import ResultCard from "../components/ResultCard";
-import Button from "./Button";
 import Link from "next/link";
 
 const ResultPage = () => {
@@ -10,22 +9,25 @@ const ResultPage = () => {
     { id: 4, date: "01/10/2023" },
   ];
   return (
-    <div className="bg-zinc-950 p-4 text-slate-400">
-      <h1 className=" text-gray-50">Hi Marco</h1>
-      <h2 className="bg-orange-600 text-gray-50">
-        Check your <strong className="text-gray-900">History</strong>
-      </h2>
-      <ul className="text-orange-600">
+    <div className="flex flex-col text-center mx-2 my-4 md:my-10">
+      <h1 className="text-5xl p-4 mb-6 text-center">Hi Marco!</h1>
+      <div className=" bg-orange-600 text-3xl md:mx-auto font-extrabold p-6">
+        <h2 className="mb-4">Check your</h2>
+        <h2 className="text-gray-900 ">History</h2>
+      </div>
+
+      <div className="">
+      <ul className="text-orange-600 py-20  text-bold ">
         {results.map((result) => (
           <li key={result.id}>
             <ResultCard result={result} />
           </li>
         ))}
-        <li>
-          <Link href="/">Home</Link>
-        </li>
       </ul>
-      <Button />
+      </div>
+      <Link href="/" className="bg-orange-600 mx-auto text-gray-900 py-1 px-8 rounded-lg text-center hover:bg-orange-400 md:text-2xl md:px-12 md:py-4">
+        Back
+      </Link>
     </div>
   );
 };
